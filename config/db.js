b8a8e6ca98f39cd6b396ad4f00979c8e03a92fed
timothyferrell <tj.ferrell@gmail.com>
2017-02-13 09:01:47 -0700

    Updated user password errors. Also changed the database to use sqllite (for local development)

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 var Sequelize = require('sequelize');
+/* //This is more for production
 var sequelize = new Sequelize('nodeTest','root','',{
     'host': '127.0.0.1',
     'dialect':'mysql',
@@ -7,6 +8,17 @@ var sequelize = new Sequelize('nodeTest','root','',{
         min: 0,
         idle: 10000
     }
+});
+*/
+var sequelize = new Sequelize('nodeTest', 'root','',{
+    'host': 'localhost',
+    'dialect': 'sqlite',
+    'pool': {
+        max: 10,
+        min: 0,
+        idle: 10000
+    },
+    'storage': './database/database.sqlite'
 });
 
 var db = {
